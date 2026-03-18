f30b379161d061ca5d6b96deca715c9a6e0b6499
Te-tsu-ya <145094090+Te-tsu-ya@users.noreply.github.com>
2026-03-18 16:50:54 +0900

    Update post.py

--- post.py
+++ post.py
@@ -1,5 +1,27 @@
+import os
+import requests
+from requests_oauthlib import OAuth1
+
+# 認証情報
+API_KEY = os.environ["X_API_KEY"]
+API_SECRET = os.environ["X_API_SECRET"]
+ACCESS_TOKEN = os.environ["X_ACCESS_TOKEN"]
+ACCESS_SECRET = os.environ["X_ACCESS_SECRET"]
+
+# news.txt読み込み
 with open("news.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-print("投稿内容👇")
-print(text)
+# 認証
+auth = OAuth1(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
+
+# 投稿
+url = "https://api.twitter.com/2/tweets"
+response = requests.post(
+    url,
+    auth=auth,
+    json={"text": text}
+)
+
+print(response.status_code)
+print(response.text)
